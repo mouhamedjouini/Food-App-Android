b5b68d2e8dd7cbe8b2543d6d4359f67dfb665e8c
mouhamedjouini <mouhadje@gmail.com>
2024-05-02 14:32:08 +0100

    Layout-of-Principal-Layout-fixed

---
+++ app/src/main/java/com/example/projectandroid/Domain/Foods.java
@@ -0,0 +1,129 @@
+package com.example.projectandroid.Domain;
+
+public class Foods {
+      private int CategoryId;
+      private String Description;
+     private Boolean BestFood;
+     private int Id;
+    private String LocationId;
+    private double Price;
+    private String ImagePath;
+    private int PriceId;
+    private double Star;
+    private int TimeId;
+    private int numberInCart;
+    private int TimeValue;
+    private String Title;
+
+    public Foods() {
+    }
+
+    @Override
+    public String toString() {
+        return  Title ;
+    }
+
+    public int getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        CategoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Boolean getBestFood() {
+        return BestFood;
+    }
+
+    public void setBestFood(Boolean bestFood) {
+        BestFood = bestFood;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getLocationId() {
+        return LocationId;
+    }
+
+    public void setLocationId(String locationId) {
+        LocationId = locationId;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+    public int getPriceId() {
+        return PriceId;
+    }
+
+    public void setPriceId(int priceId) {
+        PriceId = priceId;
+    }
+
+    public double getStar() {
+        return Star;
+    }
+
+    public void setStar(double star) {
+        Star = star;
+    }
+
+    public int getTimeId() {
+        return TimeId;
+    }
+
+    public void setTimeId(int timeId) {
+        TimeId = timeId;
+    }
+
+    public int getNumberInCart() {
+        return numberInCart;
+    }
+
+    public void setNumberInCart(int numberInCart) {
+        this.numberInCart = numberInCart;
+    }
+
+    public int getTimeValue() {
+        return TimeValue;
+    }
+
+    public void setTimeValue(int timeValue) {
+        TimeValue = timeValue;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+}
